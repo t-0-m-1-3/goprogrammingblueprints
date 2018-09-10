894a5b38e0de987c81cf5133c6517091a3668a4a
t0m <t0m@localhost.localdomain>
2018-09-09 20:04:57 -0400

    chapter 1 working webserver from template

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -1,49 +1,33 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"net/http"
-	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
-
-	"github.com/matryer/goblueprints/chapter1/trace"
 )
 
-// templ represents a single template
+//templ represents a single template
 type templateHandler struct {
 	once     sync.Once
 	filename string
 	templ    *template.Template
 }
 
-// ServeHTTP handles the HTTP request.
+// ServeHTTP handles the HTTP requests
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+	t.templ.Execute(w, nil)
 }
 
 func main() {
-	var addr = flag.String("addr", ":8080", "The addr of the application.")
-	flag.Parse() // parse the flags
-
-	r := newRoom()
-	r.tracer = trace.New(os.Stdout)
-
+	// root
 	http.Handle("/", &templateHandler{filename: "chat.html"})
-	http.Handle("/room", r)
-
-	// get the room going
-	go r.run()
-
 	// start the web server
-	log.Println("Starting web server on", *addr)
-	if err := http.ListenAndServe(*addr, nil); err != nil {
-		log.Fatal("ListenAndServe:", err)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
-
 }
