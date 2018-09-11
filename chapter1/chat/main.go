fa90bc96195e1cee79dbdc946df095990841f049
t0m <t0m@localhost.localdomain>
2018-09-10 22:54:48 -0400

    added newRoom() to main() and the room route

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -56,8 +56,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	// root
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// get the room going
+	go r.run()
 	// start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
